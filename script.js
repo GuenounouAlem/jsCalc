36f1471cf7f2e3b9199afbb9a3c0225da73ce2ba
GuenounouAlem <123456789+slayershad05@gmail.com>
2024-09-23 12:17:38 +0100

    Made it works

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ------------------- Basic functions -------------//
 
 function add(a, b) {
-    return +a + +b
+    return Number(a) + Number(b)
 }
 function sub(a, b) {
     return +a - +b
@@ -33,5 +33,68 @@ function operate(operator, a, b) {
             return 0
             break;
     }
+}
+
+// ----------------- Handle Display ----------------- //
+
+const keys = document.querySelector('.keys')
+
+keys.addEventListener('click', clicKey)
+
+let displayValue = ""
+
+function updateDisplay() {
+    const display = document.querySelector('.display')
+    display.value = displayValue
+  }
+
+const operators = ['-', '+', '/', '*']
+
+function clear() {
+    displayValue = ""
+    updateDisplay()
+}
+
+function getOperator(str) {
+    for (const char of str) {
+        if (operators.includes(char)) return char
+    }
+}
+
+function getNums(str, operator) {
+    return str.split(operator)
+}
+
+function input(val) {
+    switch (val) {
+        case 'C':
+            clear()
+            break;
+        case '=':
+            const operator = getOperator(displayValue)
+            console.log(operator);
+            
+            const [firstNum, secondNum] = getNums(displayValue, operator)
+            console.log(firstNum);
+            console.log(secondNum);
+            
+            
+            const result = operate(operator, firstNum, secondNum)
+            console.log(result);
+            
+            displayValue = result
+            updateDisplay()
+            break;
+        default:
+            displayValue += val
+            updateDisplay()
+            break;
+    }
+}
+
+function clicKey(event) {
+    let target = event.target
+    input(target.innerText)
+    
 }
 
